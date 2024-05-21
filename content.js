d463004e17ae5c7c2ec102fca039fae2dcfce1e6
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-05-21 22:58:34 +0530

    refactor: add functions to avoid code repetition

--- content.js
+++ content.js
@@ -1,89 +1,142 @@
-const container = document.createElement("div");
-const tabsListContainer = document.createElement("ul");
-const searchInput = document.createElement("input");
+// const tabsListContainer = document.createElement("ul");
+// const searchInput = document.createElement("input");
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "openPopup") {
+    const containerStyles = {
+      backgroundColor: "#36454F",
+      color: "white",
+      width: "50%",
+      height: "fit-content",
+      padding: "1em",
+      borderRadius: "10px",
+      position: "absolute",
+      top: "20%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      zIndex: "1000000000",
+      overflow: "hidden",
+      boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+      display: "flex",
+      flexDirection: "column",
+      gap: "1em",
+      margin: "0",
+    };
+
+    const tabsListContainerStyles = {
+      width: "100%",
+      height: "100%",
+      margin: "0",
+      display: "flex",
+      gap: ".5em",
+      flexDirection: "column",
+    };
+
+    const searchInputStyles = {
+      width: "100%",
+      border: "1px solid #E4D00A",
+      backgroundColor: "black",
+      borderRadius: "6px",
+      padding: "1em",
+      color: "white",
+      outline: "none",
+      fontSize: "1rem",
+    };
+
+    const searchInputAttributes = {
+      type: "search",
+      placeholder: "Find tab",
+    };
+
+    const tabListContainerAttributes = {
+      class: "tab-list-container",
+    };
+
+    const containerAttributes = {
+      id: "search-container-001",
+    };
+
     const tabsOpen = request.window;
     const body = document.querySelector("body");
+    const container = createElement("div");
+    const tabsListContainer = createElement("ul");
+    const searchInput = createElement("input");
 
+    addStyleToElement(container, containerStyles);
+    addStyleToElement(tabsListContainer, tabsListContainerStyles);
+    addStyleToElement(searchInput, searchInputStyles);
 
-
-    tabsListContainer.style.width = "100%";
-    tabsListContainer.style.height = "100%"
-    tabsListContainer.style.background = "pink"
-
-    container.style.width = "50%";
-    container.style.height = "fit-content"
-    container.style.padding = "1em"
-    container.style.borderRadius = "10px"
-    container.style.position = "absolute"
-    container.style.top = "20%"
-    container.style.left = "50%"
-    container.style.transform = "translate(-50%, -50%)"
-    container.style.zIndex = "1000000000"
-    container.style.overflow = "hidden"
-    container.style.backgroundColor = "#36454F"
-    container.style.boxShadow = "rgba(0, 0, 0, 0.35) 0px 5px 15px"
-
-    searchInput.type = "search"
-    searchInput.placeholder = "Find tabs"
-    searchInput.style.border = "1px solid #E4D00A"
-    searchInput.style.width = "100%"
-    searchInput.style.backgroundColor = "black"
-    searchInput.style.borderRadius = "6px"
-    searchInput.style.padding = ".5em 1em"
-    searchInput.style.color = "white"
-    searchInput.style.outline = "none"
-
+    addAttributeToElement(searchInput, searchInputAttributes);
+    addAttributeToElement(tabsListContainer, tabListContainerAttributes);
+    addAttributeToElement(container, containerAttributes);
 
     container.append(searchInput);
     container.append(tabsListContainer);
-    body.appendChild(container);
+
+    if (!body.lastChild.id && body.lastChild.id !== "search-container-001") {
+      body.appendChild(container);
+      listTab(tabsOpen);
+    }
 
     closeSearchInput(body, container);
-    listTab(tabsOpen)
 
-    searchInput.addEventListener('input', (e) => {
+    searchInput.addEventListener("input", (e) => {
       tabsOpen.forEach((tab) => {
         if (tab.title.toLowerCase().includes(e.target.value.toLowerCase())) {
           tabsListContainer.innerHTML = "";
           const tabList = document.createElement("li");
-          tabList.style.color = "white"
+          tabList.style.color = "white";
           tabsListContainer.appendChild(tabList);
           tabList.innerText = tab.title;
-
+        } else if (e.target.value == "") {
+          listTab(tabsOpen);
+        } else {
+          tabsListContainer.innerHTML = "Tab not found";
+          tabsListContainer.style.color = "white";
         }
-        else if (e.target.value == "") {
-          listTab(tabsOpen)
-        }
-        else {
-          tabsListContainer.innerHTML = "Tab not found"
-          // tabsListContainer.style.textAlign = "center"
-          tabsListContainer.style.color = "white"
-        }
-      })
-
-    })
+      });
+    });
     sendResponse({ status: "Feature toggled" });
   }
   return true;
 });
 
-
 function listTab(tabs) {
   tabs.forEach((tab) => {
-    const tabList = document.createElement("li");
-    tabList.style.color = "white"
+    const tabList = createElement("li");
+    addStyleToElement(tabList, { listStyle: "none", padding: "0 1em", cursor: "pointer" });
+    const tabsListContainer = accessElement(".tab-list-container");
+    tabList.style.color = "white";
     tabsListContainer.append(tabList);
     tabList.innerText = tab.title;
-  })
+  });
+}
+
+function createElement(element) {
+  return document.createElement(element);
+}
+
+function addStyleToElement(element, styles) {
+  for (const key in styles) {
+    element.style[key] = styles[key];
+  }
+}
+
+function addAttributeToElement(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
 }
 
 function closeSearchInput(parentElement, childElement) {
-  document.addEventListener('keyup', (e) => {
+  document.addEventListener("keyup", (e) => {
     if (e.key === "Escape") {
       parentElement.removeChild(childElement);
     }
-  })
+  });
 }
+
+function accessElement(identifier) {
+  return document.querySelector(identifier);
+}
+
